3100dc494022ed5a7860eb393c54794646f9626e
Nalipas <smoreno2014@gmail.com>
2024-09-18 03:24:34 -0700

    adds refactored code for completed application

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,72 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    loginUser: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: { savedBooks: input },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        if (!updatedUser) {
+          throw new Error(`Couldn't find user with this id!`);
+        }
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
